408acbd90963c09929376fbadb9cde5ad7ca8131
huaanth <hua.anth@gmail.com>
2024-08-03 21:42:44 -0400

    Added routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
+
 export default function Home() {
   return (
   <div>
    <p className ="text-3xl font-bold text-amber-500">
      Bonfire
     </p>
-    <Button> Click me</Button>
+    <Button > Click me</Button>
     </div>
   );
 }
