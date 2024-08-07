0fead4a9e830f102ba9ae01f91c5454575c2185e
huaanth <hua.anth@gmail.com>
2024-08-06 23:52:56 -0400

    added a mysql database the inital modal

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,26 @@
+import { initalProfile } from "@/lib/inital-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { Initalmodal } from "@/components/modals/inital-modal";
+
+const SetupPage = async() => {
+    const profile = await initalProfile();
+
+    const server = await db.server.findFirst({
+        where:{
+            members:{
+                some:{
+                    profileId: profile.id
+                }
+            }
+
+        }
+    });
+
+    if (server){
+        return redirect('/servers/${server.id}');
+    }
+    return <Initalmodal />
+}
+ 
+export default SetupPage;
