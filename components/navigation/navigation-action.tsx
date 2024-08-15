e889c60dd07a3126c233103f5d5d05e2460049e4
huaanth <hua.anth@gmail.com>
2024-08-15 19:44:48 -0400

    Forgot to put the onClose

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -14,7 +14,7 @@ export const NavigationAction = () =>{
             side= "right"
             align= "center"
             label = "Add campsite">
-                <button onClick= {() => onOpen("createServer")} className="group">
+                <button onClick= {() => onOpen("createServer")} className="group flex items-center">
                     <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px]
                     group-hover:rounded-[16px] transition-all overflow-hidden items-center
                     justify-center bg-background dark:bg-amber-500 group-hover:bg-yellow-300">
