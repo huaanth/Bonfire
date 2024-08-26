80759f37165d3dd2b4b73b0e4379be79992773db
huaanth <hua.anth@gmail.com>
2024-08-26 14:13:53 -0400

    can see the channels and members now

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -32,7 +32,7 @@ export const ServerSection = ({
             {role !== MemberRole.GUEST && sectionType === "channels" && (
                 <ActionTooltip label="Create tent" side ="top">
                     <button
-                    onClick={() => onOpen("createChannel")}
+                    onClick={() => onOpen("createChannel", { channelType })}
                     className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400
                      dark:hover:text-zinc-300 transition"
                     >
@@ -42,7 +42,7 @@ export const ServerSection = ({
                 </ActionTooltip>
             )}
             {role === MemberRole.ADMIN && sectionType === "members" && (
-                <ActionTooltip label="Create tent" side ="top">
+                <ActionTooltip label="Manage Campers" side ="top">
                 <button
                 onClick={() => onOpen("members", { server })}
                 className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -9,6 +9,7 @@ import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { ServerSection } from "./server-section";
 import { ServerChannel } from "./server-channel";
+import { ServerMember } from "./server-member";
 
 interface ServerSidebarProps{
     serverId: string;
@@ -123,14 +124,77 @@ export const ServerSideBar = async ({
                         role={role}
                         label="Text Tents"
                         />
-                        {textChannels.map((channel) => (
-                            <ServerChannel
-                            key = {channel.id}
-                            channel ={channel}
-                            role={role}
-                            server={server}
-                            />
+                        <div className="space-y-[2px]">
+                            {textChannels.map((channel) => (
+                                <ServerChannel
+                                key = {channel.id}
+                                channel ={channel}
+                                role={role}
+                                server={server}
+                                />
+                            ))}
+
+                        </div>
+                    </div>
+                )}
+                {!!audioChannels?.length && (
+                    <div className="mb-2">
+                        <ServerSection
+                        sectionType="channels"
+                        channelType={ChannelType.AUDIO}
+                        role={role}
+                        label="Voice Tents"
+                        />
+                        <div className="space-y-[2px]">
+                            {audioChannels.map((channel) => (
+                                <ServerChannel
+                                key = {channel.id}
+                                channel ={channel}
+                                role={role}
+                                server={server}
+                                />
+                            ))}
+                        </div>
+
+                    </div>
+                )}
+                {!!videoChannels?.length && (
+                    <div className="mb-2">
+                        <ServerSection
+                        sectionType="channels"
+                        channelType={ChannelType.VIDEO}
+                        role={role}
+                        label="Video Tents"
+                        />
+                        <div className="space-y-[2px]">
+                            {videoChannels.map((channel) => (
+                                <ServerChannel
+                                key = {channel.id}
+                                channel ={channel}
+                                role={role}
+                                server={server}
+                                />
+                            ))}
+                        </div>
+
+                    </div>
+                )}
+                {!!members?.length && (
+                    <div className="mb-2">
+                        <ServerSection
+                        sectionType="members"
+                        role={role}
+                        label="Campers"
+                        server ={server}
+                        />
+                        <div className="space-y-[2px]">
+                        {members.map((member) => (
+                            <ServerMember
+                             key = {member.id}
+                             member = {member}
+                             server = {server}/>
                         ))}
+                        </div>
 
                     </div>
                 )}
