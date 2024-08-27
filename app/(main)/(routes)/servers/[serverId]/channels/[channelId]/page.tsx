6f4c53d0dbfb051afb38539aa42a623ea8ed3722
huaanth <hua.anth@gmail.com>
2024-08-27 17:04:09 -0400

    Chat input and socket were implemented

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -3,6 +3,7 @@ import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 
 
 interface ChannelIdPageProps{
@@ -46,6 +47,15 @@ const ChannelIdPage = async ({
             serverId={channel.serverId}
             type="channel"
             />
+            <div className="flex-1"> Future messages </div>
+            <ChatInput
+            name = {channel.name}
+            type= "channel"
+            apiUrl= "/api/socket/messages"
+            query={{
+                channelId: channel.id,
+                serverId: channel.serverId,
+            }} />
         </div>
      );
 }
